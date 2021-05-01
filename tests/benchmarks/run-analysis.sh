84d2988e6579fa1cbbc4f12fef9be05f8d2308d1
abhishek@llvm <abhishek@llvm>
2021-04-30 21:03:14 -0400

    fix: run scripts

--- tests/benchmarks/run-analysis.sh
+++ tests/benchmarks/run-analysis.sh
@@ -8,7 +8,7 @@ for var in "$@"; do
     echo "Pre-Transformation Analysis:"
     valgrind --tool=memcheck --leak-check=yes --show-reachable=yes "./$var-m2r.out"
     echo ""
-    opt -load=./dominators.so -load=./heap_analysis.so -load=./transform.so -transform "./$var-m2r.bc" -o "./$var-m2r-trans.bc"
+    opt -load=../../src/dominators.so -load=../../src/heap_analysis.so -load=../../src/transform.so -transform "./$var-m2r.bc" -o "./$var-m2r-trans.bc"
     llc -relocation-model=pic -filetype=obj "./$var-m2r-trans.bc"
     gcc "$var-m2r-trans.o" -o "$var-m2r-trans.out"
     echo "Post-Transformation Analysis:"
